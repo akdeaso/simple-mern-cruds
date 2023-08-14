459001bc99333f9d8ace4282e7e6a90460585868
AKDEAS\akdea <akdeaso@gmail.com>
2023-08-14 22:20:18 +0700

    home and detail done

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,33 +1,61 @@
-import { Link } from "react-router-dom";
-import './index.scss';
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import "./index.scss";
 
 const Detail = () => {
+  const params = useParams();
+  const [product, setProduct] = useState([]);
+
+  const fetchProduct = async () => {
+    try {
+      const response = await axios.get(
+        `https://crowded-cyan-sweatshirt.cyclic.app/api/v4/product/${params.id}`
+      );
+      setProduct(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, [params.id]);
+
   return (
     <div className="main">
-      <Link to="/" className="btn btn-primary">Kembali</Link>
+      <Link to="/" className="btn btn-primary">
+        Kembali
+      </Link>
 
       <table className="table">
         <tbody>
           <tr>
             <td>ID</td>
-            <td>: asdasdasdasd</td>
+            <td>: {product._id}</td>
           </tr>
           <tr>
             <td>Name</td>
-            <td>: Laptop</td>
+            <td>: {product.name}</td>
           </tr>
           <tr>
             <td>Price</td>
-            <td>: Rp. 20.000.000</td>
+            <td>: Rp {product.price}</td>
           </tr>
           <tr>
             <td>Stock</td>
-            <td>: 10</td>
+            <td>: {product.stock}</td>
+          </tr>
+          <tr>
+            <td>Photo</td>
+            <td>
+              : <img src={product.image_url} />
+            </td>
           </tr>
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
 export default Detail;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,47 +1,125 @@
-import { Link } from 'react-router-dom';
-import './index.scss';
+import { Link } from "react-router-dom";
+import "./index.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
-  return(
+  const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [notFoundMessage, setNotFoundMessage] = useState("");
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get(
+        "https://crowded-cyan-sweatshirt.cyclic.app/api/v4/product"
+      );
+      setProducts(response.data);
+      setLoading(false);
+      setNotFoundMessage("");
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        setProducts([]);
+        setNotFoundMessage("No products found.");
+      } else {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+  };
+
+  const searchProducts = async () => {
+    if (search === "") {
+      fetchProducts();
+    } else {
+      try {
+        const response = await axios.get(
+          `https://crowded-cyan-sweatshirt.cyclic.app/api/v4/product?search=${search}`
+        );
+        setProducts(response.data);
+        setLoading(false);
+        if (response.data.length === 0) {
+          setNotFoundMessage("No products found.");
+        } else {
+          setNotFoundMessage("");
+        }
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+          setProducts([]);
+          setNotFoundMessage("No products found.");
+        } else {
+          console.log(error);
+        }
+        setLoading(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    searchProducts();
+  }, [search]);
+
+  return (
     <div className="main">
-      <Link to="/tambah" className="btn btn-primary">Tamah Produk</Link>
+      <Link to="/tambah" className="btn btn-primary">
+        Tambah Produk
+      </Link>
       <div className="search">
-        <input type="text" placeholder="Masukan kata kunci..."/>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Masukan kata kunci..."
+        />
       </div>
       <table className="table">
         <thead>
           <tr>
-            <th>ID</th>
+            <th>No.</th>
             <th>Name</th>
             <th className="text-right">Price</th>
             <th className="text-center">Action</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Laptop</td>
-            <td className="text-right">RP. 20.000.000</td>
-            <td className="text-center">
-              <Link to="/detail" className="btn btn-sm btn-info">Detail</Link>
-              <Link to="/edit" className="btn btn-sm btn-warning">Edit</Link>
-              <Link to="#" className="btn btn-sm btn-danger">Delete</Link>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Monitor</td>
-            <td className="text-right">RP. 10.000.000</td>
-            <td className="text-center">
-              <Link to="/detail" className="btn btn-sm btn-info">Detail</Link>
-              <Link to="/edit" className="btn btn-sm btn-warning">Edit</Link>
-              <Link to="#" className="btn btn-sm btn-danger">Delete</Link>
-            </td>
-          </tr>
+          {loading ? (
+            <tr>
+              <td>Loading...</td>
+            </tr>
+          ) : products.length === 0 || notFoundMessage ? (
+            <tr>
+              <td>{notFoundMessage}</td>
+            </tr>
+          ) : (
+            products.map((product, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{product.name}</td>
+                <td className="text-right">RP {product.price}</td>
+                <td className="text-center">
+                  <Link
+                    to={`/detail/${product._id}`}
+                    className="btn btn-sm btn-info"
+                  >
+                    Detail
+                  </Link>
+                  <Link
+                    to={`/edit/${product._id}`}
+                    className="btn btn-sm btn-warning"
+                  >
+                    Edit
+                  </Link>
+                  <Link to="#" className="btn btn-sm btn-danger">
+                    Delete
+                  </Link>
+                </td>
+              </tr>
+            ))
+          )}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
